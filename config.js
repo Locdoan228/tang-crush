757e1592da6b939e73f63256b9d5f6fd515c488c
Locdoan228 <86753533+Locdoan228@users.noreply.github.com>
2021-07-01 12:16:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    introTitle: 'Chào bạn!',
+    introDesc: `Mình có điều này muốn hỏi bạn nè. Nhớ trả lời thật lòng nha <3`,
+    btnIntro: 'OK nha',
+    title: 'Bạn thích mình phải không nào <3',
+    desc: 'Thoát khỏi trang này là bạn thích mình đó nha :)',
+    btnYes: 'Thích nhiều lắm <3',
+    btnNo: 'Không, bớt mơ lại đi :(',
+    question:'Tại sao bạn lại thích mình thế <3',
+    btnReply: 'Gửi cho tui nha <3',
+    reply: 'Do mình thấy bạn đẹp trai với lại dễ thương á <33333333',
+    mess: 'Mình biết mà, mình thấy bạn đẹp gái với cute nữa đó. Mình thích bạn nhiều lắm <3',
+    messDesc: 'Vô ib mình đi nào <3',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'http://fb.com/locdoan97' 
 }
